a60dfe612f0527adaf93a29321f6a6b6fc8e9fc1
Yousef Macer <yousefmacer@gmail.com>
2024-09-09 16:17:56 -0400

    Signup/Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,7 +3,15 @@ import Ticker from "../../components/Ticker/Ticker";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Login.css";
 import TwitterAuth from "../../components/Twitter/TwitterAuth";
+import { useState } from "react"; // Import useState
+
 const Login = () => {
+	const [isLogin, setIsLogin] = useState(true); // State to toggle between login and signup
+
+	const toggleLoginSignup = () => {
+		setIsLogin(!isLogin); // Toggle state
+	};
+
 	return (
 		<div>
 			<Navbar />
@@ -14,6 +22,23 @@ const Login = () => {
 					<img src={xLogo} alt="X Logo" className="x-gif" />{" "}
 				</div>
 			</div>
+
+			<div>
+				<label>
+					<input type="checkbox" onChange={toggleLoginSignup} />
+					{isLogin ? "Switch to Signup" : "Switch to Login"}
+				</label>
+			</div>
+
+			<div className="credentials">
+				{" "}
+				{/* New container for credentials */}
+				<input type="text" placeholder="Username" /> {/* Username field */}
+				{!isLogin && <input type="email" placeholder="Email" />}{" "}
+				{/* Email field for signup */}
+				<input type="password" placeholder="Password" /> {/* Password field */}
+			</div>
+
 			<div className="twitter-auth-container">
 				<TwitterAuth />
 			</div>
