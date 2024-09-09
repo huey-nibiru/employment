1b5d75b4e0bff7517ca9fd3f642c7a036a801583
Yousef Macer <yousefmacer@gmail.com>
2024-09-08 20:58:34 -0400

    Profile Navigation

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../../components/Navbar/Navbar";
 import "./Profile.css";
 import Bio from "../../components/Bio/Bio";
+import ProfileNav from "../../components/ProfileNav/ProfileNav";
 
 // In your render method or functional component
 
@@ -8,13 +9,17 @@ const Profile = () => {
 	return (
 		<div>
 			<Navbar />
-			<Bio
-				name="Cyberking"
-				profilePicture="/path/to/profile-pic.jpg"
-				basicInfo="Real Nigga | NYC"
-				description="There are no peasants in the kingdom of Heaven."
-				rating={4.5}
-			/>
+			{/* <div className="bioContainer">
+				<Bio
+					name="Cyberking"
+					profilePicture="/path/to/profile-pic.jpg"
+					basicInfo="Real Nigga | NYC"
+					description="There are no peasants in the kingdom of Heaven."
+					rating={4.5}
+				/>
+			</div>
+			*/}
+			<ProfileNav />
 		</div>
 	);
 };
