6c8ee40b29490d7f6571593c8765cda4feadd232
Yousef Macer <yousefmacer@gmail.com>
2024-09-17 01:13:20 -0400

    node update

--- src/client.tsx
+++ src/client.tsx
@@ -1,7 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.SUPABASE_URL as string; // Assert supabaseUrl as string
-const supabaseKey = process.env.SUPABASE_KEY as string; // Assert supabaseKey as string
+
+const supabaseUrl = process.env.VITE_SUPABASE_URL as string; // Assert as string
+const supabaseKey = process.env.VITE_SUPABASE_KEY as string; // Assert as string
+
+if (typeof process === "undefined") {
+	throw new Error(
+		"Process is not defined. Ensure you're running in a Node environment."
+	);
+}
+
 if (!supabaseKey) {
 	throw new Error("Supabase key is not defined"); // Ensure supabaseKey is defined
 }
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
