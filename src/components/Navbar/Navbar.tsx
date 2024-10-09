26a7c53aa0623715ba21af7585bd7401a8d655fc
Yousef Macer <yousefmacer@gmail.com>
2024-10-09 14:55:21 -0400

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar: React.FC = () => {
 						</>
 					) : (
 						<Link to="/Register" className="navbar-link">
-							Register/Login
+							Login | Register
 						</Link>
 					)}
 				</div>
