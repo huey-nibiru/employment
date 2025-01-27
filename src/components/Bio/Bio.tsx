4e7e391dfb8ac506bd57c37f063dbbb8c7eb038d
Yousef Macer <yousefmacer@gmail.com>
2025-01-26 23:42:42 -0500

    Update Dashboard

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -5,6 +5,7 @@ import { CiSettings } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import { supabase } from "../../client";
 import { ThreeDE } from "../ThreeDe/ThreeDe";
+import sol from "../../assets/solana.png";
 
 const Bio: React.FC = () => {
 	return (
@@ -21,8 +22,16 @@ const Bio: React.FC = () => {
 			>
 				<Avatar src="path/to/image.jpg" alt="Anon" />
 				<Typography variant="h6">Anon</Typography>
-				<Typography variant="body1">Sol: 10.000</Typography>
-				<Typography variant="body1">$Employment: 30000000.00</Typography>
+
+				<Box display="flex" alignItems="center">
+					<img
+						src={sol}
+						alt="Solana"
+						style={{ width: 20, height: 20, marginRight: 5 }}
+					/>
+					<Typography variant="body1">10.000</Typography>
+				</Box>
+				<Typography variant="body1">$EMPLOYMENT: 30000000.00</Typography>
 			</Box>
 		</div>
 	);
